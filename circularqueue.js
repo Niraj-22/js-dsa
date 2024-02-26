36d7cb6532b9cb188009a473c1e86f0a2e093c6d
Niraj <niraj22chordia@gmail.com>
2024-02-26 13:06:12 +0530

    Changes in circular queue

--- circularqueue.js
+++ circularqueue.js
@@ -1,42 +1,42 @@
-let myCircularQueue = function (k) {
+let MyCircularQueue = function (k) {
   this.queue = [];
   this.size = k;
 };
-myCircularQueue.prototype.enqueue = function (value) {
+MyCircularQueue.prototype.Enqueue = function (value) {
   if (this.size === this.queueSize) return false;
   this.queue.push(value);
   return true;
 };
-myCircularQueue.prototype.dequeue = function () {
+MyCircularQueue.prototype.Dequeue = function () {
   if (this.isEmpty()) return false;
   this.queue.shift();
   return true;
 };
-myCircularQueue.prototype.front = function () {
-  if (this.isEmpty()) return "queue empty";
+MyCircularQueue.prototype.Front = function () {
+  if (this.isEmpty()) return -1;
   return this.queue[0];
 };
-myCircularQueue.prototype.rear = function () {
-  if (this.isEmpty()) return "queue empty";
+MyCircularQueue.prototype.Rear = function () {
+  if (this.isEmpty()) return -1;
   return this.queue[this.queueSize() - 1];
 };
 
-myCircularQueue.prototype.queueSize = function () {
+MyCircularQueue.prototype.queueSize = function () {
   return this.queue.length;
 };
-myCircularQueue.prototype.isEmpty = function () {
+MyCircularQueue.prototype.isEmpty = function () {
   return this.queueSize() === 0;
 };
-myCircularQueue.prototype.isFull = function () {
+MyCircularQueue.prototype.isFull = function () {
   return this.queueSize() === this.size;
 };
 
-const cirQueue = new myCircularQueue(3);
-console.log("🚀 ~ cirQueue.enqueue(3);:", cirQueue.enqueue(3));
-console.log("🚀 ~ cirQueue.enqueue(3);:", cirQueue.enqueue(5));
-console.log("🚀 ~ cirQueue.enqueue(3);:", cirQueue.enqueue(7));
-console.log("🚀 ~ cirQueue.dequeue(3);:", cirQueue.dequeue());
-console.log(cirQueue.front());
-console.log(cirQueue.rear());
+const cirQueue = new MyCircularQueue(3);
+console.log("🚀 ~ cirQueue.enqueue(3);:", cirQueue.Enqueue(3));
+console.log("🚀 ~ cirQueue.enqueue(3);:", cirQueue.Enqueue(5));
+console.log("🚀 ~ cirQueue.enqueue(3);:", cirQueue.Enqueue(7));
+console.log("🚀 ~ cirQueue.dequeue(3);:", cirQueue.Dequeue());
+console.log(cirQueue.Front());
+console.log(cirQueue.Rear());
 console.log(cirQueue.isEmpty());
 console.log(cirQueue.isFull());
