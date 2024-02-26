f6db788a2e0a7d08cae6268842a59b670877cf5d
Niraj <niraj22chordia@gmail.com>
2024-02-26 15:20:13 +0530

    Sliding window

---
+++ sliding-window.js
@@ -0,0 +1,18 @@
+const maxSlidingWindow = (nums, k) => {
+  const result = [];
+  const deque = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (deque.length > 0 && deque[0] <= i - k) {
+      deque.shift();
+    }
+    while (nums[deque[deque.length - 1]] < nums[i]) {
+      deque.pop();
+    }
+    deque.push(i);
+    if (i >= k - 1) {
+      result.push(nums[deque[0]]);
+    }
+  }
+  return result;
+};
+console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3));
